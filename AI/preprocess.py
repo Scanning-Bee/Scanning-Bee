277917070872478522f82b7d7b48662082abbd38
BUKET NAZ ZEREN <e2449098@ceng.metu.edu.tr>
2023-12-19 19:17:54 +0000

    Separate preprocess functions

--- AI/preprocess.py
+++ AI/preprocess.py
@@ -11,14 +11,24 @@ def convolve(image, kernel, threshold):
 
     return normalized_result
 
-
 def contrast_stretching(img):
+
+    # if (0 <= pix and pix <= r1): 
+    #     return (s1 / r1)*pix 
+    # elif (r1 < pix and pix <= r2): 
+    #     return ((s2 - s1)/(r2 - r1)) * (pix - r1) + s1 
+    # else: 
+    #     return ((255 - s2)/(255 - r2)) * (pix - r2) + s2 
+    
+
     min_intensity = 0
     max_intensity = 255
     min_pixel = np.min(img)
     max_pixel = np.max(img)
 
-    stretched_img = (img - min_pixel) / (max_pixel - min_pixel) * (max_intensity - min_intensity) + min_intensity
+    stretched_img = (img - min_pixel) / (max_pixel - min_pixel) * (
+        max_intensity - min_intensity
+    ) + min_intensity
     stretched_img = np.uint8(stretched_img)
 
     return stretched_img
@@ -29,22 +39,86 @@ def plot_img_hist(img):
     hist = hist.ravel()
 
     plt.figure(figsize=(8, 6))
-    plt.title('Grayscale Image Histogram')
-    plt.xlabel('Pixel Value')
-    plt.ylabel('Frequency')
-    plt.plot(hist, color='black')
+    plt.title("Grayscale Image Histogram")
+    plt.xlabel("Pixel Value")
+    plt.ylabel("Frequency")
+    plt.plot(hist, color="black")
     plt.xlim(0, 256)
     plt.grid(True)
     plt.show()
+    plt.close()
+
 
 def find_first_peak(img):
-
     q_int = 8
     # Calculate the histogram
     hist, bins = np.histogram(img.flatten(), bins=256 // q_int, range=[0, 256])
 
     # Calculate the 1st and 2nd derivatives
     first_derivative = np.diff(hist)
+    second_derivative = np.diff(first_derivative)
+
+    # Find indices where the 1st derivative changes sign
+    zero_crossings_first = (
+        np.where(np.diff(np.sign(first_derivative)))[0] + 1
+    )  # 0-based index
+
+    # Find indices where the 2nd derivative changes sign
+    zero_crossings_second = (
+        np.where(np.diff(np.sign(second_derivative)))[0] + 2
+    )  # 0-based index
+
+    # Determine local maxima using the 2nd derivative
+    local_maxima_indices = zero_crossings_second[
+        second_derivative[zero_crossings_second - 1] < 0
+    ]
+
+    first_peak_value = (local_maxima_indices[0] + 2) * q_int
+
+    return first_peak_value
+
+def preprocess_dark_img(img):
+
+    blur_kernel_size = 11
+    open_kernel_size =15
+    
+    blurred_image = cv2.medianBlur(img, blur_kernel_size)
+    better_light = contrast_stretching(blurred_image)
+
+    dark_thresh = find_first_peak(better_light)
+
+    _, binary_img = cv2.threshold(better_light, dark_thresh, 255, cv2.THRESH_BINARY_INV)
+
+    kernel = np.ones((open_kernel_size, open_kernel_size), np.uint8)
+
+    # Apply opening operation
+    opening_result = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
+
+    return opening_result
+
+
+def preprocess_light_img(img):
+
+    blur_kernel_size=11
+    open_kernel_size=15
+    inverse_img = cv2.bitwise_not(img)
+    blurred_image = cv2.medianBlur(inverse_img, blur_kernel_size)
+
+    # plot_img_hist(blurred_image)
+    dark_thresh = find_first_peak(blurred_image)
+    filtered_img = np.zeros_like(blurred_image)
+    filtered_img[blurred_image < dark_thresh] = 128
+
+    better_light = contrast_stretching(filtered_img)
+
+    _, binary_img = cv2.threshold(better_light, dark_thresh, 255, cv2.THRESH_BINARY)
+
+    kernel = np.ones((open_kernel_size, open_kernel_size), np.uint8)
+
+    # Apply opening operation
+    opening_result = cv2.morphologyEx(binary_img, cv2.MORPH_OPEN, kernel)
+
+    return opening_result
     second_derivative = np.diff(first_derivative)
 
     # Find indices where the 1st derivative changes sign
