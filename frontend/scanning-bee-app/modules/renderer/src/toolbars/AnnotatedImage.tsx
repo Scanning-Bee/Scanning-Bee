791811d74eda86ddbae102e9880129e1a9297f87
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2023-12-28 23:36:32 +0300

    make annotations draggable

--- frontend/scanning-bee-app/modules/renderer/src/toolbars/AnnotatedImage.tsx
+++ frontend/scanning-bee-app/modules/renderer/src/toolbars/AnnotatedImage.tsx
@@ -2,6 +2,7 @@ import Annotation from '@frontend/models/annotation';
 import CellType from '@frontend/models/cellType';
 import {
     addAnnotation,
+    mutateAnnotation,
     setActiveAnnotations,
     setAnnotationAsActive,
     useActiveAnnotationIds,
@@ -12,6 +13,7 @@ import { getFileName } from '@frontend/utils/fileNameUtils';
 import { isMac } from '@frontend/utils/platform';
 import { UUID } from 'crypto';
 import React from 'react';
+import Draggable from 'react-draggable';
 import { useDispatch } from 'react-redux';
 
 export const AnnotatedImage = (props: { shownImageUrl: string }) => {
@@ -37,7 +39,7 @@ export const AnnotatedImage = (props: { shownImageUrl: string }) => {
     };
 
     return (
-        <span>
+        <span style={{ height: '540px' }}>
             <img
                 src={shownImageUrl}
                 alt='Annotated image'
@@ -87,30 +89,18 @@ export const AnnotatedImage = (props: { shownImageUrl: string }) => {
                 }}
             />
             {imageAnnotations.map((annotation) => {
-                const centerX = annotation.center[0] / 2;
-                const centerY = annotation.center[1] / 2;
                 const radius = annotation.radius / 2;
+                const centerX = annotation.center[0] / 2 - radius;
+                const centerY = annotation.center[1] / 2 - radius;
 
                 const isActive = activeAnnotationIds.includes(annotation.id);
 
                 return (
                     <div
                         key={annotation.id}
-                        className='flex-center noselect'
-                        style={{
-                            position: 'absolute',
-                            left: `${leftOffset + centerX - radius}px`,
-                            top: `${centerY - radius}px`,
-                            width: `${radius * 2}px`,
-                            height: `${radius * 2}px`,
-                            border: `3px solid ${CellTypeColours[annotation.cell_type]}`,
-                            borderRadius: '50%',
-                            color: CellTypeColours[annotation.cell_type],
-                            backgroundColor: isActive ? '#00FF0044' : 'transparent',
-                        }}
-                        onClick={(e) => {
+                        onMouseDown={(e) => {
                             e.stopPropagation();
-
+                            console.log('mane');
                             if ((isMac() && e.metaKey) || (!isMac() && e.ctrlKey)) {
                                 dispatch(setAnnotationAsActive({ id: annotation.id, active: !isActive }));
                             } else {
@@ -120,7 +110,46 @@ export const AnnotatedImage = (props: { shownImageUrl: string }) => {
                             }
                         }}
                     >
-                        {annotation.cell_type}
+                        { /* @ts-ignore */ }
+                        <Draggable
+                            position={{ x: centerX, y: centerY }}
+                            onStart={() => {
+                                setTimeout(() => {
+                                    dispatch(setActiveAnnotations([annotation.id]));
+                                }, 20);
+                            }}
+                            onStop={(e, pos) => {
+                                e.stopPropagation();
+
+                                const newX = (pos.x);
+                                const newY = (pos.y);
+
+                                const mutation = {
+                                    id: annotation.id,
+                                    mutations: { center: [newX * 2 + radius * 2, newY * 2 + radius * 2] },
+                                };
+
+                                dispatch(mutateAnnotation(mutation));
+                            }}
+                        >
+                            <div
+                                key={annotation.id}
+                                className='flex-center noselect'
+                                style={{
+                                    position: 'absolute',
+                                    left: `${leftOffset}px`,
+                                    top: 0,
+                                    width: `${radius * 2}px`,
+                                    height: `${radius * 2}px`,
+                                    border: `3px solid ${CellTypeColours[annotation.cell_type]}`,
+                                    borderRadius: '50%',
+                                    color: CellTypeColours[annotation.cell_type],
+                                    backgroundColor: isActive ? '#00FF0044' : 'transparent',
+                                }}
+                            >
+                                {annotation.cell_type}
+                            </div>
+                        </Draggable>
                     </div>
                 );
             })}
