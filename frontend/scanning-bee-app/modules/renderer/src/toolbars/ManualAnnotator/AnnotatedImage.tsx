3f9ab630bb9e20db616a7122c877535bb8705172
y4nci <baran.yanci@metu.edu.tr>
2024-02-06 00:43:34 +0300

    if brush mode on, create the new annotation with selected type

--- frontend/scanning-bee-app/modules/renderer/src/toolbars/ManualAnnotator/AnnotatedImage.tsx
+++ frontend/scanning-bee-app/modules/renderer/src/toolbars/ManualAnnotator/AnnotatedImage.tsx
@@ -77,7 +77,7 @@ export const AnnotatedImage = (props: { shownImageUrl: string, mode: string, bru
                         const newAnnotation = new Annotation({
                             center: [2 * x, 2 * y],
                             radius: 80,
-                            cell_type: CellType.NOT_CLASSIFIED,
+                            cell_type: mode === 'brush' ? brushCellType : CellType.NOT_CLASSIFIED,
                             poses: [0, 0],
                             source_name: getFileName(shownImageUrl),
                             timestamp: 0,
