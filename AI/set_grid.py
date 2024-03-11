209719543c75fbb0a8de7a656935f0a22d9f283e
buket naz zeren <e2449098@ceng.metu.edu.tr>
2024-03-12 00:37:43 +0300

    clean up and bug fixes

--- AI/set_grid.py
+++ AI/set_grid.py
@@ -1,6 +1,7 @@
-from .detect import *
-from .preprocess import *
-
+from detect import *
+from preprocess import *
+import random
+from typing import List, Tuple
 FOUR_OVER_ROOT3 = 2.309401077
 TWO_OVER_ROOT3 = 1.154700538
 THREE_OVER_ROOT3 = 1.732050808
@@ -8,7 +9,7 @@ ROOT3 = 1.732050808
 TAN120 = 1.732050808
 
 
-def draw_parallel_grid(plot_img, detected_circles, slope=TAN120):
+def draw_parallel_grid(plot_img: np.ndarray, detected_circles: List[Tuple[int, int, int]], slope: float=TAN120) -> np.ndarray:
     height, width, _ = plot_img.shape
 
     the_circle = detected_circles[0]
@@ -76,21 +77,29 @@ def draw_parallel_grid(plot_img, detected_circles, slope=TAN120):
     return plot_img
 
 
-def get_patches(img, detected_circles, cell_space=0.15, error_margin=0.5):
+def get_patches(img: np.ndarray, detected_circles: List[Tuple[int, int, int]], cell_space: float=0.15, error_margin: float=0.7, show_grid: bool = False) -> Tuple[List[Tuple[int, int, int, int]], List[Tuple[int, int, int, int]]]:
+    '''
+    Takes an image for plotting, the list of circles detected in the first stage, cell space and error_margin parameters.
+    Returns a tuple of two lists: The first one is the patch corners with the error margin added, the second one is patch corners without the error margin.
+    TODO: Further experimentation on cell_space, error_margin needed.
+    '''
+
     height, width, _ = img.shape
 
-    if detected_circles is None:
-        return None
+    if len(detected_circles) == 0:
+        return [], []
 
     # get the most confidence circle
     x, y, r = [int(i) for i in detected_circles[0]]
-    r = int(np.mean(detected_circles, axis=0)[2])
 
-    # first grid
+    space_bw_two_rows = int(r * 3 * TWO_OVER_ROOT3 * (1 + cell_space))
+    space_bw_two_cols = int(r * 2 * (1 + cell_space))
+    
+    #### Create first grid, this is not slided, should surround anchor circle
     horizontal_lines = []
     vertical_lines = []
 
-    # rows
+    # traverse over rows down, start from the anchor circle level
     line = y
     while line < height:
         up = int(line - r)
@@ -98,18 +107,19 @@ def get_patches(img, detected_circles, cell_space=0.15, error_margin=0.5):
         if up < 0 or down > height:
             break
         horizontal_lines.append((up, down))
-        line += int(r * 3 * TWO_OVER_ROOT3 * (1 + cell_space))
+        line += space_bw_two_rows
 
-    line = y - int(r * 3 * TWO_OVER_ROOT3 * (1 + cell_space))
+    # traverse over rows up, start two rows above anchor circle
+    line = y - space_bw_two_rows
     while line > 0:
         up = int(line - r)
         down = int(line + r)
         if up < 0 or down > height:
             break
         horizontal_lines.append((up, down))
-        line -= int(r * 3 * TWO_OVER_ROOT3 * (1 + cell_space))
+        line -= space_bw_two_rows
 
-    # columns
+    # columns, go right by starting from anchor circle column
     column = x
     while column < width:
         left = int(column - r)
@@ -118,23 +128,23 @@ def get_patches(img, detected_circles, cell_space=0.15, error_margin=0.5):
             break
         vertical_lines.append((left, right))
         # vertical_lines.append((left,right)) 
-        column += int(r * 2 * (1 + cell_space))
+        column += space_bw_two_cols
 
-    column = x - int(r * 2 * (1 + cell_space))
+    # columns, go left starting from two level left of anchor circle
+    column = x - space_bw_two_cols
     while column > 0:
         left = int(column - r)
         right = int(column + r)
         if left < 0 or right > width:
             break
         vertical_lines.append((left, right))
-        # vertical_lines.append((left,right)) 
-        column -= int(r * 2 * (1 + cell_space))
+        column -= space_bw_two_cols
 
-    # second, slided grid
+    ### At every second row, slide the columns, calculate this grid
     slided_horizontal_lines = []
     slided_vertical_lines = []
 
-    # rows
+    # traverse over rows down, start from one level below the anchor circle
     line = y + THREE_OVER_ROOT3 * r
     while line < height:
         up = int(line - r)
@@ -142,18 +152,19 @@ def get_patches(img, detected_circles, cell_space=0.15, error_margin=0.5):
         if up < 0 or down > height:
             break
         slided_horizontal_lines.append((up, down))
-        line += int(r * 3 * TWO_OVER_ROOT3 * (1 + cell_space))
+        line += space_bw_two_rows
 
-    line = y + THREE_OVER_ROOT3 * r - int(r * 3 * TWO_OVER_ROOT3 * (1 + cell_space))
+    # traverse over rows up, start from one level above the anchor circle
+    line = y + THREE_OVER_ROOT3 * r - space_bw_two_rows
     while line > 0:
         up = int(line - r)
         down = int(line + r)
         if up < 0 or down > height:
             break
         slided_horizontal_lines.append((up, down))
-        line -= int(r * 3 * TWO_OVER_ROOT3 * (1 + cell_space))
+        line -= space_bw_two_rows
 
-    # columns
+    # columns, go right by starting from one right column of anchor
     column = x + r
     while column < width:
         left = int(column - r)
@@ -161,18 +172,17 @@ def get_patches(img, detected_circles, cell_space=0.15, error_margin=0.5):
         if left < 0 or right > width:
             break
         slided_vertical_lines.append((left, right))
-        # vertical_lines.append((left,right)) 
-        column += int(r * 2 * (1 + cell_space))
+        column += space_bw_two_cols
 
-    column = x + r - int(r * 2 * (1 + cell_space))
+    # columns, go left by starting from one right column of anchor
+    column = x + r - space_bw_two_cols
     while column > 0:
         left = int(column - r)
         right = int(column + r)
         if left < 0 or right > width:
             break
         slided_vertical_lines.append((left, right))
-        # vertical_lines.append((left,right)) 
-        column -= int(r * 2 * (1 + cell_space))
+        column -= space_bw_two_cols
 
     first_grid = []
     second_grid = []
@@ -180,139 +190,211 @@ def get_patches(img, detected_circles, cell_space=0.15, error_margin=0.5):
     first_tight = []
     second_tight = []
 
-    # create and draw grids
+    # uncomment to visualize grid
+    if show_grid:
+        img_draw = img.copy()
+        img_draw = cv2.circle(img_draw, (x,y), r, (0,0,255), 2)
+
+    # using calculates lines, find corners for patches in first grid
     for row in horizontal_lines:
         for column in vertical_lines:
             up = int(row[0] - error_margin * r)
             down = int(row[1] + error_margin * r)
             left = int(column[0] - error_margin * r)
             right = int(column[1] + error_margin * r)
-            # cv2.line(img,(left,up),(right,up),(255,255,0),3)
-            # cv2.line(img,(right,up),(right,down),(255,255,0),3)
-            # cv2.line(img,(right,down),(left,down),(255,255,0),3)
-            # cv2.line(img,(left,down),(left,up),(255,255,0),3)
+
+            if up < 0 or down > height or left < 0 or right > width:
+                continue
+
+            if show_grid:
+                cv2.rectangle(img_draw, (left, up), (right, down), tuple(np.random.random(size=3) * 256), 2)
+
             first_grid.append((left, right, up, down))
             first_tight.append((int(column[0]), int(column[1]), int(row[0]), int(row[1])))
 
+    # using calculated and slided lines, find patch corners for second grid
     for row in slided_horizontal_lines:
         for column in slided_vertical_lines:
             up = int(row[0] - error_margin * r)
             down = int(row[1] + error_margin * r)
             left = int(column[0] - error_margin * r)
             right = int(column[1] + error_margin * r)
-            # cv2.line(img,(left,up),(right,up),(0,255,0),3)
-            # cv2.line(img,(right,up),(right,down),(0,255,0),3)
-            # cv2.line(img,(right,down),(left,down),(0,255,0),3)
-            # cv2.line(img,(left,down),(left,up),(0,255,0),3)
+
+            if up < 0 or down > height or left < 0 or right > width:
+                continue
+
+            if show_grid:
+                cv2.rectangle(img_draw, (left, up), (right, down), tuple(np.random.random(size=3) * 256), 2)
+
             second_grid.append((left, right, up, down))
             second_tight.append((int(column[0]), int(column[1]), int(row[0]), int(row[1])))
 
-    return img, first_grid, second_grid, first_tight, second_tight
+    ## combine two grids on one list
+    grid_corners = first_grid + second_grid
+    tight_corners = first_tight + second_tight
+
+    if show_grid:
+        cv2.imshow('the_grids', img_draw)
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            cv2.destroyWindow('the_grids')
+
+    return grid_corners, tight_corners
 
 
-def detect_second_stage(img, first_grid, second_grid, first_detected):
+def detect_second_stage(img: np.ndarray, patch_corners: List[Tuple[int, int, int, int]], first_detected: Tuple[int, int, int], show_patches:bool = False):
+    '''
+    img: An image
+    Takes an image, a patch_corner list and an array of detected_circles.
+    If a patch is empty, runs local search on that patch.
+    Returns the results as a list, also returns the image patches for visualization purposes.
+    '''
     newly_detected = []
-
     patches = []
 
-    radius = int(np.mean(first_detected, axis=0)[2])
-
-    for l, r, u, d in first_grid:
+    # check every patch
+    for l, r, u, d in patch_corners:
         inside = False
+
+        # compare patches with previously found circles to see if a circle exists in this patch
         for circle in first_detected:
-            x, y, _ = circle
+            x, y, detection_radius = circle
             if l < x and x < r and u < y and y < d:
                 inside = True
                 break
 
+        # if no circle is in this patch
         if not inside:
             patch = img[u:d, l:r]
             patches.append(patch)
-            try:
-                circles = detect_circle_on_clip(patch)[:1]
-            except:
-                circles = None
-            if circles is not None:
+            circles = detect_circle_on_clip(patch)[:1]
+            if circles.size > 0:
                 for circle in circles:
-                    x, y, _ = circle
-                    newly_detected.append([int(x + l), int(y + u), int(radius)])
+                    x, y, detection_radius = circle
+                    # x and y are coordinates on patch, to adjust to whole image add l or u
+                    newly_detected.append([int(x + l), int(y + u), int(detection_radius)])
 
-    for l, r, u, d in second_grid:
-        inside = False
-        for circle in first_detected:
-            x, y, _ = circle
-            if l < x and x < r and u < y and y < d:
-                inside = True
-                break
+    if show_patches:
+        # Assuming 'patches' is a list containing the image patches
+        # and 'patch_corners' contains the corners of each patch
 
-        if not inside:
-            patch = img[u:d, l:r]
-            patches.append(patch)
-            try:
-                circles = detect_circle_on_clip(patch)[:1]
-            except:
-                circles = None
-            if circles is not None:
-                for circle in circles:
-                    x, y, _ = circle
-                    newly_detected.append([int(x + l), int(y + u), int(radius)])
+        # Calculate the number of rows and columns for the subplot grid
+        num_patches = len(patches)
+        num_cols = 6  # You can adjust the number of columns as needed
+        num_rows = int(np.ceil(num_patches / num_cols))
 
-    return newly_detected, patches
+        # Create a subplot grid
+        fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 15))
 
+        # Check if axes is a NumPy array and flatten if needed
+        if isinstance(axes, np.ndarray):
+            axes = axes.flatten()
 
-def draw_circles(img, detected_circles, color=(0, 255, 255)):
-    if detect_circles is not None:
-        for x,y,radius in detected_circles:
-            cv2.circle(img, (int(x),int(y)), int(radius), color, 7)
+        # Plot each patch in a separate subplot
+        for i, (patch, (l, r, u, d)) in enumerate(zip(patches, patch_corners)):
+            ax = axes[i]
+            ax.imshow(patch, cmap='gray')  # You can adjust the colormap as needed
+            ax.axis('off')
 
-    return img
+        # Adjust layout for better visualization
+        plt.tight_layout()
+        plt.show()
 
-
-def tile_circles(img, first_grid, second_grid, first_detected):
-    newly_detected = []
-
-    radius = int(np.mean(first_detected, axis=0)[2])
-
-    for l, r, u, d in first_grid:
-        inside = False
-        for circle in first_detected:
-            x, y, _ = circle
-            if l < x and x < r and u < y and y < d:
-                inside = True
-                break
-
-        if not inside:
-            newly_detected.append([int(l + radius), int(u + radius), int(radius)])
-
-    for l, r, u, d in second_grid:
-        inside = False
-        for circle in first_detected:
-            x, y, _ = circle
-            if l < x and x < r and u < y and y < d:
-                inside = True
-                break
-
-        if not inside:
-            newly_detected.append([int(l + radius), int(u + radius), int(radius)])
 
     return newly_detected
 
 
-def filter_circles(first_detected, second_detected, default_radius=70):
-    all_circles = first_detected
+def draw_circles(img: np.ndarray, detected_circles: List[Tuple[int, int, int]], color: Tuple[int, int, int] = (0, 255, 255), verbose:bool = False):
+    if detected_circles is not None:
+        for x,y,radius in detected_circles:
+            cv2.circle(img, (int(x),int(y)), int(radius), color, 7)
 
-    def check_circle(x, y, radius):
-        # check of proximity
-        for x_c, y_c, _ in all_circles:
-            distance = np.sqrt((x_c - x) ** 2 + (y_c - y) ** 2)
-            if distance < 1.5 * default_radius:
-                return False
-        return True
+    if verbose:
+        cv2.imshow('circles', img)
+        if cv2.waitKey(0) & 0xFF == ord('q'):
+            cv2.destroyWindow('circles')
 
-    filtered_second = []
-    for x, y, r in second_detected:
-        if check_circle(x, y, r):
-            filtered_second.append([x, y, r])
-            all_circles = np.append(all_circles, [[x, y, r]], axis=0)
+    return img
 
-    return filtered_second
+
+def tile_circles(img: np.ndarray, patch_corners: List[Tuple[int, int, int, int]], found_circles: List[Tuple[int, int, int]]) -> List[Tuple[int, int, int]]:
+    '''
+    For every patch, check if a circle exists inside, if not assume there is one, 
+    Returns a list of (x,y,r) tuples
+    '''
+
+    assumed_circles = []
+
+    ## make radius same as the anchor circle's radius
+    radius = int(found_circles[0][2])
+
+    # check every patch
+    for l, r, u, d in patch_corners:
+        inside = False
+        # compare with every circle found so far
+        for circle in found_circles:
+            x, y, _ = circle
+            if l < x and x < r and u < y and y < d:
+                inside = True
+                break
+
+        # if no circle found inside patch, assume there is one
+        if not inside:
+            assumed_circles.append([int(l + radius), int(u + radius), int(radius)])
+
+    return assumed_circles
+
+def filter_and_add_circles(first_detected: List[Tuple[int, int, int]], second_detected: List[Tuple[int, int, int]], default_radius: int=70):
+    """
+    Filter circles in 'second_detected' based on proximity to circles in 'first_detected' 
+    and proximity to other circles in 'second_detected'. Concatenate with first detected, return an array containing
+    all circles.
+
+    Parameters:
+    - first_detected (List): Array of circles detected in the first stage.
+    - second_detected (List): Array of circles detected in the second stage.
+    - default_radius (int): Default radius for circles.
+
+    Returns:
+    - numpy.ndarray: Filtered circles from the second detection stage together with the circles from the first stage.
+    """
+
+    ### As the first step, compare circles in second_detected with each other
+    # Calculate distances between circles in 'second'
+
+    first_detected = np.array(first_detected)
+    second_detected = np.array(second_detected)
+
+    if not second_detected.size:
+        return first_detected
+    points_second = second_detected[:, :2]
+    differences_second = points_second[:, np.newaxis, :] - points_second
+    distances_second = np.sqrt(np.sum(differences_second**2, axis=-1))
+
+    # Calculate the sum of radii for 'second'
+    radii_sum_second = second_detected[:, 2] + second_detected[:, 2][:, np.newaxis]
+
+    # Create a boolean mask for circles in 'second' that satisfy the condition
+    mask_second = distances_second < radii_sum_second
+
+    # Apply the mask to filter out circles in 'second'
+    filtered_circles_second = second_detected[mask_second.sum(axis=1) == 1]
+
+    ### As second step, compare filtered_circles_second with first detected
+    # calculate distances between circles in 'first' and filtered circles in 'second'
+    points_second = filtered_circles_second[:, :2]
+    differences_first_second = points_second[:, np.newaxis, :] - first_detected[:, :2]
+
+    distances_first_second = np.sqrt(np.sum(differences_first_second**2, axis=-1))
+
+    # Calculate the sum of radii for 'first' and filtered circles in 'second'
+    radii_sum_first_second = first_detected[:, 2] + filtered_circles_second[:, 2][:, np.newaxis]
+
+    # Create a boolean mask for circles in 'first' and filtered circles in 'second' that satisfy the condition
+    mask_first_second = distances_first_second < radii_sum_first_second
+
+    # Apply the mask to filter out circles in 'filtered_circles_second'
+    filtered_circles_final = filtered_circles_second[mask_first_second.sum(axis=1) == 0]
+    all_circles = np.concatenate([first_detected,filtered_circles_final],axis=0)
+
+    return all_circles
+
--- AI/test.py
+++ AI/test.py
@@ -1,7 +1,8 @@
-from .set_grid import *
+from set_grid import *
 import glob
 from ultralytics import YOLO
-
+from typing import List, Tuple
+from PIL import Image 
 
 # This function belongs to Ege Keyvan, we try to improve this detection, so we use this as a baseline for testing our
 # detections
@@ -64,54 +65,119 @@ def test_detection():
                 plt.savefig(f'./results/grid_search/d{default}f{min_r}t{max_r}.png')
 
 
-def test_lines(image_path, occlude=False):
+manual_bbox_paths = {"..\datasets\2023-10-08-12-01-05\image_651.jpg ":"..\datasets\no_bee_test\image_651.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_2355.jpg ":"..\datasets\no_bee_test\image_2355.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_2612.jpg ":"..\datasets\no_bee_test\image_2612.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_3193.jpg ":"..\datasets\no_bee_test\image_3193.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_3389.jpg ":"..\datasets\no_bee_test\image_3389.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_4208.jpg ":"..\datasets\no_bee_test\image_4208.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_4640.jpg ":"..\datasets\no_bee_test\image_4640.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_4715.jpg ":"..\datasets\no_bee_test\image_4715.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_6116.jpg ":"..\datasets\no_bee_test\image_6116.txt",
+                     "..\datasets\2023-10-08-12-01-05\image_7632.jpg ":"..\datasets\no_bee_test\image_7632.txt",
+                     }
+
+def test_lines(image_path: str, occlude:bool = False, detection_model_path:str = 'AI/models/bee_detect_models/bee_model_v2.pt',cell_space:float = 0.4, error_margin:float = 0.7, intersect_threshold:float = 0.3) -> List[Tuple[int, int, int]]:
+    '''
+    The latest version of the circle detection code, made for the CENG49X Design Project Course.
+    - image_path is the path of the object image
+    - occlude is a flag for choosing whether the bee occlusion mask should be enabled. It is highly
+    
+    recommended not to enable it for now, because the segmentation model must be strengthened.
+    
+    TODO update the docstring when the segmentation model is updated.
+
+    Returns a list of (x, y, r) tuples, that being the detected circles.
+    '''
+
+    ## read original image in gray_scale, used for first and second stage processing
     sample_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    height, width = sample_image.shape
 
+    ## get a rgb copy of original image for model input and plotting
     plot_img = cv2.cvtColor(sample_image, cv2.COLOR_GRAY2RGB)
-    point_img = plot_img.copy()
+
+    ## this is a binary mask which will be used to check intersection of bees with cells
+    mask = np.ones_like(sample_image) * 255
+    
+    if occlude:
+        try:
+            ## using yolov8 bee detection model
+            model = YOLO(detection_model_path)
+            # print("Bee detection model loaded")
+            prediction = model(plot_img,verbose= True)[0]
+            scores = prediction.boxes.conf
+            bboxes = prediction.boxes.xyxy
+
+            ## mark where bees are on binary mask
+            for score,bbox in zip(scores,bboxes):
+                if score < 0.4:
+                    continue
+                x1,y1,x2,y2 = list(map(int,bbox))
+                cv2.rectangle(mask,(x1,y1),(x2,y2),color = (0,0,0), thickness = -1)
 
 
-    first_detected_circles, _, _ = return_hough(sample_image)
-    all_detected_circles = first_detected_circles
+            ## uncomment to use manual_annotation_files
+            # if image_path in manual_bbox_paths.keys():
+            #     print("Manual annotation loaded")
+            #     annot_file = open(manual_bbox_paths[image_path])
+            #     lines = annot_file.readlines()
 
-    # plot_img = draw_parallel_grid(plot_img,first_detected_circles)
+            #     for line in lines:
+            #         #class x_center y_center width height normalized
+            #         cls, x_center, y_center, w, h =  line.split(" ")
+            #         x_center = x_center * width
+            #         y_center = y_center * height
+            #         w = w * width
+            #         h = h * height
+            #         x1 = int(x_center - w)
+            #         x2 = int(x_center + w)
+            #         y1 = int(y_center - h)
+            #         y2 = int(y_center + h)
+            #         cv2.rectangle(mask,(x1,y1),(x2,y2),color = (0,0,0), thickness = -1)
 
-    if first_detected_circles is None:
+        ## if model cannot be found, do not change the mask, continue without occlusion
+        except Exception as error:
+            print(error)
+            print("Bee detection model could not be loaded, please fix the above error and try again")
+
+    ## first stage of detection, uses hough transform to detect circles, see function for preprocessing
+    first_stage_circles = detect_circles_using_hough_transform(sample_image,use_dark=True,use_light=False)
+    
+    ## filter out the circles from first stage that are occluded by bees
+    filtered_first_stage_circles = filter_intersecting_circles(first_stage_circles, mask)
+
+    ## if no circles found, return earlt since we have no anchor
+    if len(filtered_first_stage_circles) == 0:
+        # print("No anchor found, auto detection is not possible")
         return []
+
+    # Calculate the grid and patch corners using anchor circles
+    patch_corners, tight_patch_corners = get_patches(plot_img, filtered_first_stage_circles,cell_space=cell_space, error_margin=error_margin, show_grid=False)
     
-    point_img, first_grid, second_grid, first_tight, second_tight = get_patches(point_img, first_detected_circles,
-                                                                                cell_space=0.03, error_margin=0.15)
+    # Search for circles in images patches
+    second_stage_circles = detect_second_stage(sample_image, patch_corners,filtered_first_stage_circles, show_patches=False)
 
-    second_detect_circles,patches = detect_second_stage(sample_image,first_grid,second_grid,first_detected_circles)
-    
-    if second_detect_circles is not None:
-        second_detect_circles = filter_circles(first_detected_circles,second_detect_circles)
-        
-        all_detected_circles = np.vstack([all_detected_circles,second_detect_circles])
+    # Filter out second stage circles intersecting with bees
+    filtered_second_stage_circles = filter_intersecting_circles(second_stage_circles,mask,intersection_threshold=intersect_threshold)
 
-    tiled_circles = tile_circles(sample_image,first_tight,second_tight, all_detected_circles)
-    tiled_circles = filter_circles(all_detected_circles,tiled_circles)
-    
-    if tiled_circles != []:
-        all_detected_circles = np.vstack([all_detected_circles,tiled_circles])
+    # Compare circle distances, filter out if the distance is smaller than sum of radii.
+    merged_second_stage_circles = filter_and_add_circles(filtered_first_stage_circles,filtered_second_stage_circles)
 
-    # if occlude:
-    #     model = YOLO('AI/bee_segment_model.pt')
-    #     bee_mask = create_bee_mask(model,sample_image)
-    #     all_detected_circles = filter_intersecting_circles(all_detected_circles,bee_mask)
-    
-    # plot_img = draw_circles(plot_img,all_detected_circles)
+    # In the patches that we cannot find a circle,assume there is one
+    tiled_circles = tile_circles(sample_image,tight_patch_corners,merged_second_stage_circles)
 
-    # plot_img = draw_circles(plot_img,first_detected_circles,(255,255,0))
-    # plot_img = draw_circles(plot_img,second_detect_circles,(0,128,255))
-    # plot_img = draw_circles(plot_img,tiled_circles,color=(255,128,0))
+    # Of those assumed circles, remove the ones occluded by bees
+    filtered_tiled_circles = filter_intersecting_circles(tiled_circles,mask,intersection_threshold=intersect_threshold)
 
-    # plt.imshow(plot_img)
-    # plt.show()
+    # Check if tiled_circles are suitably away from other circles, if so add to final list one by one
+    merged_final_circles = filter_and_add_circles(merged_second_stage_circles,filtered_tiled_circles)
 
-    return all_detected_circles
-    
+    # Convert the NumPy array to a list of tuples, for the return
+    return_list = [tuple(row) for row in merged_final_circles]
+
+    return return_list
 
 
 if __name__ == "__main__":
-    test_lines("AI/test_images/image_1219.jpg",False)
+    print(test_lines("AI/test_images/image_1219.jpg",True))
