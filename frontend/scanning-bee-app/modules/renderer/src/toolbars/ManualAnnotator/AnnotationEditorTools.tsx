2ea720f1f66bd37c384b121127dde0b145c66050
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2024-03-21 16:43:00 +0300

    enable resizing multiple annotations

--- frontend/scanning-bee-app/modules/renderer/src/toolbars/ManualAnnotator/AnnotationEditorTools.tsx
+++ frontend/scanning-bee-app/modules/renderer/src/toolbars/ManualAnnotator/AnnotationEditorTools.tsx
@@ -21,7 +21,7 @@ const RadiusSlider = (props: {
 
     const dispatch = useDispatch();
 
-    const disabled = activeAnnotations.length !== 1;
+    const disabled = activeAnnotations.length < 1;
 
     const theme = useTheme();
 
@@ -43,10 +43,12 @@ const RadiusSlider = (props: {
                     max={200}
                     stepSize={1}
                     labelStepSize={40}
-                    onRelease={value => dispatch(mutateAnnotation({
-                        id: activeAnnotations[0].id,
-                        mutations: { radius: value },
-                    }))}
+                    onRelease={(value) => {
+                        activeAnnotations.forEach(annotation => dispatch(mutateAnnotation({
+                            id: annotation.id,
+                            mutations: { radius: value },
+                        })));
+                    }}
                     value={displayedRadius}
                     initialValue={displayedRadius}
                     onChange={value => setDisplayedRadius(value)}
