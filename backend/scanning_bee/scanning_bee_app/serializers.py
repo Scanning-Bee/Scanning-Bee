a909247865abbf02714911859c7000563085dfd8
Taner Sarp Tonay <sarptonay813@gmail.com>
2024-04-24 02:53:28 +0300

    We can now track how much this user annotated wo query

--- backend/scanning_bee/scanning_bee_app/serializers.py
+++ backend/scanning_bee/scanning_bee_app/serializers.py
@@ -12,7 +12,7 @@ class UserTypeSerializer(serializers.ModelSerializer):
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password', 'user_type']
+        fields = ['username', 'email', 'password', 'user_type', 'annotation_count']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
