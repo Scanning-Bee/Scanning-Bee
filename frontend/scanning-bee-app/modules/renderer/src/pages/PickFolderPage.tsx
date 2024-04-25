7d28023d215d59482d307451eab7dc8d426d142e
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2024-04-25 10:49:15 +0300

    fix recently opened folder section colours

--- frontend/scanning-bee-app/modules/renderer/src/pages/PickFolderPage.tsx
+++ frontend/scanning-bee-app/modules/renderer/src/pages/PickFolderPage.tsx
@@ -5,53 +5,57 @@ import { getRecentlyOpenedFolders } from '@frontend/utils/annotationUtils';
 import { getFileName, getParentFolder, shortenFileName, shortenFolderPath } from '@frontend/utils/fileNameUtils';
 import React from 'react';
 
-const RecentlyOpenedFolders = ({ folders }: { folders: string[] }) => (
-    <div
-        style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginTop: '20px',
-            width: '500px',
-        }}>
-        <p style={{ fontWeight: 'bold', fontSize: '18px' }}>Recently opened folders</p>
-        {(
-            folders.map((folder, index) => (
-                <Button
-                    key={index}
-                    text={
-                        <div
-                            style={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                alignItems: 'flex-start',
-                                justifyContent: 'space-between',
-                                width: '490px',
-                            }}
-                        >
-                            <p style={{ fontWeight: 'normal', fontSize: '14px' }} className='nomargin'>
-                                {shortenFileName(getFileName(folder), 35)}
-                            </p>
-                            <p style={{ fontWeight: '200', fontSize: '12px' }} className='nomargin'>
-                                        at {shortenFolderPath(getParentFolder(folder), 60)}
-                            </p>
-                        </div>
-                    }
-                    minimal
-                    fill
-                    onClick={() => {
-                        console.log('Opening folder:', folder);
-                        BackendInterface.getInstance().openFolderAtLocation(folder);
-                    }}
-                    style={{
-                        padding: '5px',
-                        margin: '0',
-                    }}
-                />
-            ))
-        )}
-    </div>
-);
+const RecentlyOpenedFolders = ({ folders }: { folders: string[] }) => {
+    const theme = useTheme();
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                marginTop: '20px',
+                width: '500px',
+            }}>
+            <p style={{ fontWeight: 'bold', fontSize: '18px' }}>Recently opened folders</p>
+            {(
+                folders.map((folder, index) => (
+                    <Button
+                        key={index}
+                        text={
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'flex-start',
+                                    justifyContent: 'space-between',
+                                    width: '490px',
+                                }}
+                            >
+                                <p style={{ fontWeight: 'normal', fontSize: '14px', color: theme.primaryForeground }} className='nomargin'>
+                                    {shortenFileName(getFileName(folder), 35)}
+                                </p>
+                                <p style={{ fontWeight: '200', fontSize: '12px', color: theme.secondaryForeground }} className='nomargin'>
+                                        at {shortenFolderPath(getParentFolder(folder), 35)}
+                                </p>
+                            </div>
+                        }
+                        minimal
+                        fill
+                        onClick={() => {
+                            console.log('Opening folder:', folder);
+                            BackendInterface.getInstance().openFolderAtLocation(folder);
+                        }}
+                        style={{
+                            padding: '5px',
+                            margin: '0',
+                        }}
+                    />
+                ))
+            )}
+        </div>
+    );
+};
 
 export const PickFolderPage = () => {
     const theme = useTheme();
