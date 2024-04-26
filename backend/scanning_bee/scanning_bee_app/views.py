4c09ae534bd02fdbf4777e78b0016b63c2439333
Taner Sarp Tonay <sarptonay813@gmail.com>
2024-04-26 16:49:19 +0300

    .

--- backend/scanning_bee/scanning_bee_app/views.py
+++ backend/scanning_bee/scanning_bee_app/views.py
@@ -255,7 +255,7 @@ class CellContentsByAI(ListCreateAPIView):
         cell_contents = list()
 
         for image in image_list:
-            all_detected_circles = test_lines("../../AnnotationFiles/" + image.image_name)
+            all_detected_circles = test_lines("../../AnnotationFiles/" + image.image_path)
 
             frame = Frame.objects.get(pk=1)
             user = User.objects.get(pk=1)
