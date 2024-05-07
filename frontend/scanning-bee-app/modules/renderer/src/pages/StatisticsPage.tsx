6a8ac562f461127f471d9979b12ffa4523debe22
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2024-05-07 23:09:33 +0300

    remove unnecessary plots

--- frontend/scanning-bee-app/modules/renderer/src/pages/StatisticsPage.tsx
+++ frontend/scanning-bee-app/modules/renderer/src/pages/StatisticsPage.tsx
@@ -1,8 +1,6 @@
 import { useAnnotationsFolder } from '@frontend/slices/annotationSlice';
 import { useTheme } from '@frontend/slices/themeSlice';
-import { AnnotationsByImageChart } from '@frontend/toolbars/charts/AnnotationsByImageChart';
 import { CellTypeChart } from '@frontend/toolbars/charts/CellTypeChart';
-import { TimestampChart } from '@frontend/toolbars/charts/TimestampChart';
 import { XYChart } from '@frontend/toolbars/charts/XYChart';
 import React from 'react';
 
@@ -31,16 +29,9 @@ export const StatisticsPage = () => {
             <CellTypeChart />
             <div style={{ height: '20px' }} />
 
-            <h1>Timestamps</h1>
-            <TimestampChart />
-            <div style={{ height: '20px' }} />
-
             <h1>X & Y Coordinates</h1>
             <XYChart />
             <div style={{ height: '20px' }} />
-
-            <h1>Annotations by Image</h1>
-            <AnnotationsByImageChart />
         </div>
     );
 };
