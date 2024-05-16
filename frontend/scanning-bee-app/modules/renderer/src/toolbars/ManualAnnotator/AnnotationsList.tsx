3ed07f8828b34e8f207a2524fc8fd98c3725ed58
Baran Yancı <73401941+y4nci@users.noreply.github.com>
2024-05-16 22:09:13 +0300

    replace circle annotation symbol with hexagon

--- frontend/scanning-bee-app/modules/renderer/src/toolbars/ManualAnnotator/AnnotationsList.tsx
+++ frontend/scanning-bee-app/modules/renderer/src/toolbars/ManualAnnotator/AnnotationsList.tsx
@@ -11,6 +11,7 @@ import { useBackendStatus } from '@frontend/slices/backendStatusSlice';
 import { useTheme } from '@frontend/slices/themeSlice';
 import { CellTypeColours } from '@frontend/utils/colours';
 import { getFileName } from '@frontend/utils/fileNameUtils';
+import { HexagonBase } from '@frontend/views/base/HexagonBase';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -71,11 +72,13 @@ export const AnnotationsList = () => {
                                 </span>
                                 }
                                 minimal
-                                icon={<Icon
-                                    icon='full-circle'
-                                    color={CellTypeColours[annotation.cell_type]}
-                                    style={{ margin: '0 5px 0 0' }}
-                                />}
+                                icon={<div style={{ margin: '0 5px 0 0' }}>
+                                    <HexagonBase
+                                        color={CellTypeColours[annotation.cell_type]}
+                                        height={16}
+                                        width={16}
+                                    />
+                                </div>}
                                 onClick={() => {
                                     dispatch(setActiveAnnotations([annotation.id]));
                                 }}
